5d5c99317631401f73031aadce15436f8a0d6019
SUMIT KUMAR <77963232+sumitkumar16dec@users.noreply.github.com>
2022-03-16 07:47:52 +0530

    Added solution - LeetHub

---
+++ 19. Merge three sorted arrays - GFG/19.-merge-three-sorted-arrays.cpp
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std; 
+
+
+void printVector(const vector<int>& a) 
+{ 
+    for (auto e : a) { 
+        cout << e << " "; 
+    }
+    cout<<endl;
+} 
+
+ // } Driver Code Ends
+class Solution{
+  public:
+    // A, B, and C: input sorted arrays
+    //Function to merge three sorted vectors or arrays 
+    //into a single vector or array.
+    vector<int> mergeThree(vector<int>& A, vector<int>& B, vector<int>& C) 
+    { 
+        vector<int> D(A.size()+B.size()+C.size());
+        for(int i=0;i<A.size();i++){
+            D[i]=A[i];
+        }
+        for(int i=0;i<B.size();i++){
+            D[i+A.size()]=B[i];
+        }
+        for(int i=0;i<C.size();i++){
+            D[i+A.size()+B.size()]=C[i];
+        }
+        sort(D.begin(),D.end());
+        return D;
+    } 
+
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+
+
+    int t;
+    cin>>t;
+    while(t--){
+    int n,m,o;
+    cin>>n>>m>>o;
+    vector<int> A,B,C;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        A.push_back(x);
+    }
+    
+    for(int i=0;i<m;i++){
+        int x;
+        cin>>x;
+        B.push_back(x);
+    }
+    
+    for(int i=0;i<o;i++){
+        int x;
+        cin>>x;
+        C.push_back(x);
+    }
+
+    Solution obj;
+    vector<int>ans = obj.mergeThree(A, B, C);
+    for(auto i: ans)cout << i << " ";
+    cout << "\n";
+    // obj.printVector(mergeThree(A, B, C)); 
+    
+    }
+   
+    return 0; 
+} 
+  // } Driver Code Ends
