6a24c03f92f42c45d78a234b2c3fe901814d4635
SUMIT KUMAR <77963232+sumitkumar16dec@users.noreply.github.com>
2022-04-20 08:39:39 +0530

    Added solution - LeetHub

--- Minimum indexed character - GFG/minimum-indexed-character.cpp
+++ Minimum indexed character - GFG/minimum-indexed-character.cpp
@@ -14,19 +14,19 @@ class Solution
     //Function to find the minimum indexed character.
     int minIndexChar(string str, string patt)
     {
-        unordered_map<char,int> m;
-        for(int i=0;i<str.size();i++){
-            if(m.find(str[i])==m.end()) m[str[i]]=i;
+        unordered_map<char,int> m;      // AS: O(str.size)
+        for(int i=0;i<str.size();i++){  // TC: O(str.size)
+            if (m.find(str[i])==m.end()) m[str[i]]=i;
         }
         int min_index=INT_MAX;
-        for(int i=0;i<patt.size();i++){
+        for(int i=0;i<patt.size();i++){  // O(patt.size)
             if( m.find(patt[i])!=m.end() ) min_index=min(min_index,m[patt[i]]);
         }
         if(min_index!=INT_MAX) return min_index;
         else return -1;
     }
-};
-
+};  // Overall AS: O(str.size) ,  TC: O(str.size)
+  // https://www.youtube.com/watch?v=6nWWUYNnpXM
 // { Driver Code Starts.
 
 int main()
