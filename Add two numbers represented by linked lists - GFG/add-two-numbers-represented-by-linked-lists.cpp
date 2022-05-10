539eb7642d9a9a1c41879e4f4e43308493ccda83
SUMIT KUMAR <77963232+sumitkumar16dec@users.noreply.github.com>
2022-05-10 09:07:02 +0530

    Added solution - LeetHub

--- Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.cpp
+++ Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.cpp
@@ -73,33 +73,45 @@ class Solution
     //Function to add two numbers represented by linked list.
     struct Node* addTwoLists(struct Node* first, struct Node* second)
     {
-        first=reverse(first);
-        second=reverse(second);
-        int c=0;
-        int s=0;
-        Node *temp;
-        Node *res=NULL;
-        Node *cur=NULL;
-        while(first!=NULL || second!=NULL){
-            s=c+(first?first->data:0)+(second?second->data:0);
-            c=(s>=10)?1:0;
-            s=s%10;
-            temp=new Node(s);
-            if(res==NULL) res=temp;
-            else cur->next=temp;
-            cur=temp;
-            if(first) first=first->next;
-            if(second) second=second->next;
+        first = reverse(first);             
+        second = reverse(second);            
+        
+        Node *sum=NULL;
+        int carry=0;
+        
+        //using a loop till both lists and carry gets exhausted.
+        while( first!=NULL || second!=NULL || carry!=0 )
+        {
+            //using a variable to store sum of two digits along with carry.
+            int newVal = carry;
+            
+            //if nodes are left in any list, we add their data in newVal.
+            if(first) newVal += first->data;
+            if(second) newVal += second->data;
+        
+            //updating carry.
+            carry = newVal/10;
+            
+            //using modulus to store only a single digit at that place.
+            newVal = newVal%10;             
+            
+            //creating new node which gets connected with other nodes that 
+            //we get after calculating sum of respective digits.
+            Node* newNode = new Node(newVal);
+            
+            //storing the previously made nodes in the link part of new node.
+            newNode->next = sum;
+            
+            //making the new node as the first node of all previously made node.
+            sum = newNode;
+            
+            //moving ahead in both lists.
+            if(first) first = first->next; 
+            if(second) second = second->next;
         }
-        if(c>0){
-            temp=new Node(c);
-            cur->next=temp;
-            cur=temp;
-        }
-        res=reverse(res);
-        return res;
+        return sum;
     }
-};  // https://www.youtube.com/watch?v=EvjpRN9CpDI
+};  // Dry Run for understanding
 
 
 // { Driver Code Starts.
