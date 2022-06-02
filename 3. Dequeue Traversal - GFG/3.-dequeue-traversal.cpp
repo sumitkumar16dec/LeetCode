2aff9cc47c4f03bc6028d2c64b1584004d073b3c
SUMIT KUMAR <77963232+sumitkumar16dec@users.noreply.github.com>
2022-06-02 08:02:26 +0530

    Added solution - LeetHub

---
+++ 3. Dequeue Traversal - GFG/3.-dequeue-traversal.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+//Function to traverse the Deque and print the elements of it.
+void printDeque(deque<int> Deq)
+{
+    for(auto x: Deq){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+}    
+    
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        deque<int> Deq;
+        for(int i = 0; i < n; i++)
+        {
+            int val;
+            cin>>val;
+            Deq.push_back(val);
+        }
+        printDeque(Deq);
+        
+    }
+}
+  // } Driver Code Ends
