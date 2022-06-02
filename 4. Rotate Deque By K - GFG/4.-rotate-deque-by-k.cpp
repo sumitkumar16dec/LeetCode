1afbfbc7cd144c3078b39115825c6debfc5406aa
SUMIT KUMAR <77963232+sumitkumar16dec@users.noreply.github.com>
2022-06-02 08:15:50 +0530

    Added solution - LeetHub

---
+++ 4. Rotate Deque By K - GFG/4.-rotate-deque-by-k.cpp
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+//Function to rotate deque by k places in anti-clockwise direction.
+void left_Rotate_Deq_ByK(deque<int> &deq, int n, int k)
+{
+    while(k--){
+        deq.push_back(deq.front());
+        deq.pop_front();
+    }
+}
+
+//Function to rotate deque by k places in clockwise direction.
+void right_Rotate_Deq_ByK(deque<int> &deq, int n, int k)
+{
+    while(k--){
+        deq.push_front(deq.back());
+        deq.pop_back();
+    }
+}
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        deque<int> deq;
+        for(int i = 1; i <= n; i++)
+        {
+            int val;
+            cin>>val;
+            deq.push_back(val);
+            
+        }
+        int rotateQuery, k;
+        cin>>rotateQuery>>k;
+        
+        
+        
+        if(rotateQuery == 1)
+            right_Rotate_Deq_ByK(deq, n, k);
+            
+        else left_Rotate_Deq_ByK(deq, n, k);
+        
+        
+        for(auto itr = deq.begin(); itr != deq.end(); itr++)
+            cout << *itr << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
