0ce6acb0193d46aab930e990c7b9cccffe0e7fd1
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2022-07-09 14:00:31 +0530

    Added solution - LeetHub

---
+++ Coin Change - Number of ways - GFG/coin-change-number-of-ways.cpp
@@ -0,0 +1,77 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+class Solution
+{
+    public:
+    // long long f(int coins[],int index,int value){
+        
+    //     if(index==0) return (value % coins[0] == 0);
+        
+    //     long long notTake = f(coins, index-1, value);
+    //     long long take=0;
+    //     if(coins[index]<=value) take=f(coins, index, value-coins[index]);
+    //     return take + notTake;
+        
+    // }
+    
+    long long fMem(int coins[],int index,int value, vector<vector<long long>>& dp){
+        
+        if(index==0) return (value % coins[0] == 0);
+        if(dp[index][value]!=-1) return dp[index][value];
+        
+        long long notTake = fMem(coins, index-1, value, dp);
+        long long take=0;
+        if(coins[index]<=value) take=fMem(coins, index, value-coins[index], dp);
+        return dp[index][value] = take + notTake;
+        
+    }
+    
+    //Function to find out the number of ways to use the coins to
+    //sum up to a certain required value.
+    long long numberOfWays(int coins[],int numberOfCoins,int value)
+    {
+        vector<vector<long long>> dp( numberOfCoins, vector<long long>(value+1, -1) );
+        return fMem(coins, numberOfCoins-1, value, dp);
+    }
+};
+// https://www.youtube.com/watch?v=HgyouUi11zk&t=57s
+
+// { Driver Code Starts.
+
+
+int main() {
+    
+    //taking total count of testcases
+	int testcases;
+	cin>>testcases;
+	while(testcases--)
+	{
+	    //taking value
+	    int value;
+	    cin>>value;
+	    
+	    //taking number of coins
+	    int numberOfCoins;
+	    cin>>numberOfCoins;
+	    int coins[numberOfCoins];
+	    
+	    //inserting coins to the array
+	    for(int i=0;i<numberOfCoins;i++)
+	    cin>>coins[i];
+	    Solution obj;
+	    //calling function numberOfWays
+	    cout<<obj.numberOfWays(coins,numberOfCoins,value)<<endl;
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
