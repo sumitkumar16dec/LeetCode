b646efdc77bbe46220ba5272c899778db630059d
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2022-09-30 23:20:51 +0530

    Time: 4 ms (58.08%), Space: 9.2 MB (30.49%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -4,11 +4,9 @@ public:
         int a = m-1;
         int b = n-1;
         int c = m+n-1;
-        while(b>=0){        // O(m+n)
-            if( a>=0 && nums1[a]>nums2[b]) nums1[c--]=nums1[a--];
-            else{
-                nums1[c--]=nums2[b--];
-            }
+        while(b>=0){            // O(m+n)
+            if(a>=0 && nums1[a]>nums2[b]) nums1[c--]=nums1[a--];
+            else nums1[c--]=nums2[b--];
         }
     }
 };
