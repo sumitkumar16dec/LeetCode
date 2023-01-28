285ee17092dfa62c6afe64a192c238c1cba2307d
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2023-01-28 22:57:25 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.cpp
+++ N meetings in one room - GFG/n-meetings-in-one-room.cpp
@@ -1,39 +1,37 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution
 {
     public:
-    static bool compare(pair<int,int> a, pair<int,int> b){
-        if(a.second==b.second) return a.first<b.first;
-        return (a.second < b.second);
-    }
-    
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
+    
+    static bool compare(pair<int,int> a, pair<int,int> b){
+        if(a.second==b.second) return a.first<b.first;
+        return a.second<b.second;
+    }
+    
     int maxMeetings(int start[], int end[], int n)
     {
-        vector<pair<int,int>> v(n);         // SC: O(n)
-        for(int i=0;i<n;i++) v[i]={start[i],end[i]};    // O(n)
+        vector<pair<int,int>> v(n);     // SC: O(n)
+        for(int i=0;i<n;i++) v[i]= {start[i],end[i]};   // O(n)
+        sort(v.begin(), v.end(), compare);          // O(nlogn)
         
-        sort(v.begin(), v.end(), compare);  // O(nlogn)
-        
-        int i = 0;
-        int res = 1;
-        for(int j=1; j<n; j++){             // O(n)
-            if(v[j].first > v[i].second){
-                res++;
-                i = j;
-            }
+        int i=0, cnt=1;
+        for(int j=1;j<n;j++){           // O(n)
+            if(v[i].second<v[j].first) {cnt++; i=j;}
         }
-        return res;
+        return cnt;
     }
 };
 // TC: O(n+nlogn+n)= O(nlogn), SC: O(n)
 // https://www.youtube.com/watch?v=AsbDqOauGZE
-// { Driver Code Starts.
+
+
+//{ Driver Code Starts.
 int main() {
     int t;
     cin >> t;
@@ -50,4 +48,5 @@ int main() {
         cout << ans << endl;
     }
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
