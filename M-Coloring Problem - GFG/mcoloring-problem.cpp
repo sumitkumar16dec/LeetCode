8feaef843d9f5aaecd284f507388877bfd60516b
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2023-02-16 00:32:49 +0530

    Added solution - LeetHub

--- M-Coloring Problem - GFG/mcoloring-problem.cpp
+++ M-Coloring Problem - GFG/mcoloring-problem.cpp
@@ -1,39 +1,45 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
-// Function to determine if graph can be coloured with at most M colours such
-// that no two adjacent vertices of graph are coloured with same colour.
-bool isSafe(int node, int color[], bool graph[101][101], int n, int col){
-    for(int k=0;k<n;k++){
-        if(k!=node && graph[k][node]==1 && color[k]==col) return false;
-    }
-    return true;
-}
-
-bool solve(int node, int color[], int m, int n, bool graph[101][101]){
-    if(node==n) return true;
-    
-    for(int i=1;i<=m;i++){
-        if(isSafe(node, color, graph, n, i)){
-            color[node]=i;
-            if(solve(node+1, color, m, n, graph)) return true;
-            color[node]=0;
+// } Driver Code Ends
+class Solution{
+public:
+    // Function to determine if graph can be coloured with at most M colours such
+    // that no two adjacent vertices of graph are coloured with same colour.
+    bool isSafe(int node, int color[], bool graph[101][101], int n, int col){
+        for(int k=0;k<n;k++){
+            if(k!=node && graph[k][node]==1 && color[k]==col) return false;
         }
+        return true;
     }
-    return false;
-}
+    
+    bool solve(int node, int color[], bool graph[101][101], int m, int n){
+        // base case
+        if(node==n) return true;
+        
+        for(int i=1;i<=m;i++){
+            if(isSafe(node, color, graph, n, i)){
+                color[node]=i;
+                
+                if(solve(node+1, color, graph, m, n)) return true;
+                color[node]=0;
+            }
+        }
+        return false;
+    }
+    
+    bool graphColoring(bool graph[101][101], int m, int n) {
+        int color[n] = {0};
+        if(solve(0, color, graph, m, n)) return true;
+        return false;
+    }
+};
+// TC: O(m^n), SC: O(n)
+// https://youtu.be/wuVwUK25Rfc
 
-bool graphColoring(bool graph[101][101], int m, int n) {
-    int color[n]={0};
-    if(solve(0,color,m,n,graph)) return true;
-    return false;
-}
-// https://www.youtube.com/watch?v=wuVwUK25Rfc
-
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main() {
     int t;
@@ -52,8 +58,10 @@ int main() {
             graph[a - 1][b - 1] = 1;
             graph[b - 1][a - 1] = 1;
         }
-        cout << graphColoring(graph, m, n) << endl;
+        Solution ob;
+        cout << ob.graphColoring(graph, m, n) << endl;
     }
     return 0;
 }
-  // } Driver Code Ends
+
+// } Driver Code Ends
