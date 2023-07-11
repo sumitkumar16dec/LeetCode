63bb9d99a2b7ccb3142e1ba07d911d395a319a85
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2023-07-11 12:23:42 +0530

    Added solution - LeetHub

---
+++ Kth largest element in BST - GFG/kth-largest-element-in-bst.cpp
@@ -0,0 +1,185 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+/*The Node structure is defined as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+// return the Kth largest element in the given BST rooted at 'root'
+class Solution
+{
+    public:
+    int kthLargest(Node *root, int k)
+    {
+        if(root==NULL) return -1;
+        
+        // Counting of nodes
+        int size=0;
+        Node* cur= root;
+        while(cur!=NULL){
+            if(cur->left==NULL){
+                size++;
+                cur= cur->right;
+            }
+            else{
+                Node* prev= cur->left;
+                while(prev->right && prev->right!=cur){
+                    prev= prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right= cur;
+                    cur= cur->left;
+                }
+                else{
+                    prev->right= NULL;
+                    size++;
+                    cur= cur->right;
+                }
+            }
+        }
+        
+        // Morris Inorder Traversal
+        int cnt=0, ksmall=INT_MIN;
+        cur= root;
+        while(cur!=NULL){
+            if(cur->left==NULL){
+                cnt++;
+                if(cnt==size-k+1) ksmall=cur->data;
+                cur= cur->right;
+            }
+            else{
+                Node* prev= cur->left;
+                while(prev->right && prev->right!=cur){
+                    prev= prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right= cur;
+                    cur= cur->left;
+                }
+                else{
+                    prev->right= NULL;
+                    cnt++;
+                    if(cnt==size-k+1) ksmall=cur->data;
+                    cur= cur->right;
+                }
+            }
+        }
+        return ksmall;
+    }
+};
+// TC: O(n), SC: O(1)
+// https://youtu.be/80Zug6D1_r4
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    getchar();
+    
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* head = buildTree(s);
+        
+        int k;
+        cin>>k;
+        getchar();
+        
+        Solution ob;
+        cout << ob.kthLargest( head, k ) << endl;
+    }
+    return 1;
+}
+// } Driver Code Ends
