9f81dbae61f09dc87511cca0e07580c4a303f026
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2023-10-19 14:06:12 +0530

    Time: 4 ms (43.79%), Space: 8 MB (59.07%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    void func(int ind, vector<int> &nums, vector<vector<int>> &ans){
+        // base
+        if(ind==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[i],nums[ind]);
+            func(ind+1, nums, ans);
+            swap(nums[i],nums[ind]);
+        }
+    }
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        func(0, nums, ans);
+        return ans;
+    }
+};
+// TC: O(n! * n), SC: O(n) [recursion]
+// https://youtu.be/f2ic2Rsc9pU
