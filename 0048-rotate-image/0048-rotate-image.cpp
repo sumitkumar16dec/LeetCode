5a1d31c997bab0ab2976275d1cb3128c6ddb8176
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2023-10-21 15:28:26 +0530

    Time: 0 ms (100%), Space: 7.3 MB (58.88%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        // step1: find transpose
-        for(int i=0;i<matrix.size();i++){           // O(n^2)
+        int n= matrix.size();
+        for(int i=0;i<n;i++){       // O(n^2)
             for(int j=0;j<i;j++){
-                swap(matrix[i][j], matrix[j][i]);
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
-        
-        // step2: reverse each row
-        for(int i=0;i<matrix.size();i++){           // O(n^2)
+
+        for(int i=0;i<n;i++){       // O(n^2)
             reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
-// TC: O(n^2 + n^2) , SC: O(1)
+// TC: O(n^2), SC: O(1)
 // https://youtu.be/Y72QeX0Efxw
