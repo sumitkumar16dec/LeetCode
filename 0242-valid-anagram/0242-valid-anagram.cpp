8fe601c4aaaf9782630f74ef442a6712526a4ebb
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2024-05-30 19:01:38 +0530

    Time: 8 ms (64.61%), Space: 7.9 MB (100%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+
+        for(auto i: mp){
+            if(i.second!=0) return false;
+        }
+        return true;
+    }
+};
+// TC: O(n), SC: O(n)
+// editorial solution
+// or, https://youtu.be/wKF6L9Zo8PQ
