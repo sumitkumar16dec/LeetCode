2ec5e4ea92a0e28b0885c24a90b1a180b489dd84
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2024-05-31 20:01:47 +0530

    Time: 12 ms (38.53%), Space: 16.5 MB (41.82%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int col0=1, rows=matrix.size(), columns=matrix[0].size();
-        for(int i=0;i<rows;i++){            // O(r*c)
+        int col0=1, n=matrix.size(), m= matrix[0].size();
+        for(int i=0;i<n;i++){
             if(matrix[i][0]==0) col0=0;
-            for(int j=1;j<columns;j++){
-                if(matrix[i][j]==0) matrix[i][0]=matrix[0][j]=0;
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
             }
         }
-        
-        for(int i=rows-1;i>=0;i--){            // O(r*c)
-            for(int j=columns-1;j>=1;j--){
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=1;j--){
                 if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;
             }
             if(col0==0) matrix[i][0]=0;
         }
-        
     }
 };
-// TC: O(m*n + m*n) , SC: O(1)
-// https://www.youtube.com/watch?v=M65xBewcqcI&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=8
+// TC: O(n*m), SC: O(1)
+// https://youtu.be/M65xBewcqcI
