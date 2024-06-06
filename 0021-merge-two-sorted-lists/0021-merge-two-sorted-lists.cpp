589ab728a66c77076c828eb040d99a4523e7771d
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2024-06-06 19:44:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,13 +10,18 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *dummy= new ListNode();
+        ListNode *l1= list1, *l2= list2;
+        dummy->next= l1;
+
         if(l1==NULL) return l2;
         if(l2==NULL) return l1;
-        
-        if(l1->val>l2->val) swap(l1,l2);
-        ListNode *head= l1, *temp= NULL;
-        while(l1!=NULL){
+
+        if(l2->val < l1->val) swap(l1, l2);
+
+        ListNode *temp= NULL;
+        while(l1){
             if(l1->val <= l2->val){
                 temp= l1;
                 l1= l1->next;
@@ -27,7 +32,7 @@ public:
             }
         }
         temp->next= l2;
-        return head;
+        return dummy->next;
     }
 };
 // TC: O(n1+n2), SC: O(1)
