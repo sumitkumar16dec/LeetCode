51186bc0957a9a3175246baae7ae594812116866
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2024-06-07 19:48:41 +0530

    Time: 5 ms (66.33%), Space: 12.9 MB (62.23%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,35 +17,35 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        //step 1: make copy LL while connecting it
-        Node *dummy= head, *nex= head;
-        while(dummy){
-            nex= dummy->next;
-            Node *temp= new Node(dummy->val);
-            dummy->next= temp;
-            temp->next= nex;
+        //step 1: copies banao
+        Node * temp = head;
+        while(temp){
+            Node *nex = temp->next;
+            Node *nd = new Node(temp->val);
+            temp->next= nd;
+            nd->next= nex;
 
-            dummy= nex;
+            temp= nex;
         }
 
-        //step 2: connect random pointers
-        dummy= head;
-        while(dummy){
-            if(dummy->random) dummy->next->random= dummy->random->next;
-            dummy= dummy->next->next;
+        //step 2: random pointers ko match karo
+        temp = head;
+        while(temp){
+            if(temp->random) temp->next->random = temp->random->next;
+            temp= temp->next->next;
         }
 
-        //step 3: segregate both LL
-        dummy= head; nex= head;
-        Node *nHead= new Node(0);
-        Node *copy= nHead;
-        while(dummy){
-            nex= dummy->next->next;
-            copy->next= dummy->next;
-            dummy->next= nex;
-
+        //step 3: alag karo dono LL ko
+        temp= head;
+        Node *dummy= new Node(0);
+        Node *nHead= dummy;
+        while(temp){
+            Node *nex= temp->next->next;
+            dummy->next= temp->next;
+            temp->next= temp->next->next;
+            
+            temp= nex;
             dummy= dummy->next;
-            copy= copy->next;
         }
         return nHead->next;
     }
