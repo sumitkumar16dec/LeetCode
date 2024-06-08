cd76983ead5f943c5100c7a42d14c9b5de05cdd1
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2024-06-09 03:26:03 +0530

    Time: 3 ms (79.79%), Space: 9.2 MB (94.38%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,33 +1,35 @@
 class Solution {
-private:
-    void func(int col, vector<string> &ds, vector<vector<string>> &ans, vector<int> &leftrow, vector<int> &upperdiagonal, vector<int> &lowerdiagonal, int n){
-        if(col==n) {ans.push_back(ds); return;}
+public:
+    void func(int col, vector<string> &board, vector<vector<string>> &ans, vector<int> &left, vector<int> &lowerdiagonal, vector<int> &upperdiagonal, int n){
+        if(col==n){
+            ans.push_back(board);
+            return;
+        }
 
         for(int row=0;row<n;row++){
-            if(leftrow[row]==0 && upperdiagonal[n-1 + col-row]==0 && lowerdiagonal[row+col]==0){
-                ds[row][col]='Q';
-                leftrow[row]=1;
-                upperdiagonal[n-1 + col-row]=1;
+            if(left[row]==0 && lowerdiagonal[row+col]==0 && upperdiagonal[col-row + n-1]==0){
+                board[row][col]='Q';
+                left[row]=1;
                 lowerdiagonal[row+col]=1;
+                upperdiagonal[col-row + n-1]=1;
 
-                func(col+1, ds, ans, leftrow, upperdiagonal, lowerdiagonal, n);
+                func(col+1, board, ans, left, lowerdiagonal, upperdiagonal, n);
 
-                ds[row][col]='.';
-                leftrow[row]=0;
-                upperdiagonal[n-1 + col-row]=0;
+                board[row][col]='.';
+                left[row]=0;
                 lowerdiagonal[row+col]=0;
+                upperdiagonal[col-row + n-1]=0;
             }
         }
     }
 
-public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> ds(n);
+        vector<string> board(n);
         string s(n,'.');
-        for(int i=0;i<n;i++) ds[i]=s;
-        vector<int> leftrow(n,0), upperdiagonal(2*n-1,0), lowerdiagonal(2*n-1,0);
-        func(0, ds, ans, leftrow, upperdiagonal, lowerdiagonal, n);
+        for(int i=0;i<n;i++) board[i]=s;
+        vector<int> left(n,0), lowerdiagonal(2*n-1,0), upperdiagonal(2*n-1,0);
+        func(0, board, ans, left, lowerdiagonal, upperdiagonal, n);
         return ans;
     }
 };
