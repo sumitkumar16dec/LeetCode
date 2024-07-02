fe59d9179063f078df6c96a90389f8a4aa54390e
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2024-07-02 11:08:53 +0530

    Time: 105 ms (32.18%), Space: 61.1 MB (44.58%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,30 +1,32 @@
 class Solution {
-private:
-    bool isPalindrome(string s, int st, int en){
-        while(st<=en){
-            if(s[st++]!=s[en--]) return false;
-        }
-        return true;
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> temp;
+        func(0, temp, ans, s);
+        return ans;
     }
 
-    void func(int ind, string s, vector<string> &ds, vector<vector<string>> &ans){
-        if(ind==s.size()) ans.push_back(ds);
+    void func(int ind, vector<string> &temp, vector<vector<string>> &ans, string s){
+        if(ind==s.size()){
+            ans.push_back(temp);
+            return;
+        }
 
-        for(int i=ind;i<s.size();i++){
-            if(isPalindrome(s, ind, i)){
-                ds.push_back(s.substr(ind, i-ind+1));
-                func(i+1, s, ds, ans);
-                ds.pop_back();
+        for(int i=ind; i<s.size(); i++){
+            if(isPalindrome(ind, i, s)){
+                temp.push_back(s.substr(ind,i-ind+1));
+                func(i+1, temp, ans, s);
+                temp.pop_back();
             }
         }
     }
 
-public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string> ds;
-        func(0, s, ds, ans);
-        return ans;
+    bool isPalindrome(int st, int en, string s){
+        while(st<=en){
+            if(s[st++]!=s[en--]) return false;
+        }
+        return true;
     }
 };
 // TC: O(n * 2^n)
