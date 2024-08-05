66d67524192918f8b2ff761b3c621937849a331a
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2024-08-06 00:09:18 +0530

    Time: 0 ms (100%), Space: 14.4 MB (99.67%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n= nums.size(), ind1=-1, ind2=-1;
-        // step1: find break-through point
-        for(int i=n-2;i>=0;i--){                            // O(n)
-            if(nums[i]<nums[i+1]) {ind1=i; break;}
+        int n= nums.size(), n1=-1, n2=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]) {n1=i; break;}
         }
-        
-        // step2: find with which it'll be swapped
-        if(ind1!=-1){
-            for(int j=n-1;j>=0;j--){                        // O(n)
-                if(nums[j]>nums[ind1]) {ind2=j; break;}
+
+        if(n1!=-1){
+            for(int i=n-1;i>=0;i--){
+                if(nums[i]>nums[n1]) {n2=i; break;}
             }
         }
-        
-        // step3: swap
-        if(ind1!=-1 && ind2!=-1){
-            swap(nums[ind1], nums[ind2]);                   // O(1)
-        }
-        
-        // step4: reverse
-        reverse(nums.begin()+ind1+1, nums.end());       // O(n)
-        
+
+        if(n1!=-1) swap(nums[n1], nums[n2]);
+
+        reverse(nums.begin()+n1+1, nums.end());
     }
 };
-// TC: O(n+n+n) = O(n) , SC: O(1)
+// TC: O(n), SC: O(1)
 // https://youtu.be/LuLCLgMElus
