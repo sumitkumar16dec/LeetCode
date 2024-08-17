f85d7d73d46ee898422df9911f51280d151f45f0
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2024-08-17 11:52:12 +0530

    Time: 14 ms (81.48%), Space: 26.5 MB (99.65%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int maj=-1, c=0;
+        for(int i: nums){
+            if(maj==i) c++;
+            else if(c==0) maj=i;
+            else c--;
+        }
+        return maj;
+    }
+};
+// TC: O(n) , SC: O(1)
+// https://youtu.be/AoX3BPWNnoE
+// Moore Voting Algo
