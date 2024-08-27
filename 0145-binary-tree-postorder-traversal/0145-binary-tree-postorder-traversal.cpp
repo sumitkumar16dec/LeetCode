c901ad33f96853e6f8d0efd64a552d24e6d0a98b
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2024-08-27 11:48:17 +0530

    Time: 4 ms (32.85%), Space: 10.2 MB (25.37%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,20 +11,26 @@
  */
 class Solution {
 public:
-    void postorder(TreeNode *root, vector<int> &ans){
-        if(root==NULL) return;
-
-        postorder(root->left, ans);
-        postorder(root->right, ans);
-        ans.push_back(root->val);
-    }
-
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        postorder(root, ans);
-        return ans;
+        vector<int> postorder;
+        if(root==NULL) return postorder;
+        stack<TreeNode*> st1, st2;
+        st1.push(root);
+        while(!st1.empty()){
+            TreeNode *temp= st1.top();
+            st1.pop();
+            st2.push(temp);
+            if(temp->left) st1.push(temp->left);
+            if(temp->right) st1.push(temp->right);
+        }
+
+        while(!st2.empty()){
+            postorder.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return postorder;
     }
 };
-// Recursive way
-// TC: O(n), SC: O(height)
-// https://youtu.be/COQOU6klsBg
+// Iterative way
+// TC: O(n), SC: O(2n)
+// https://www.youtube.com/watch?v=NzIGLLwZBS8
