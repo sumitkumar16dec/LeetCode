4e04917dd12fb1da15e374f77faa971cc20b8766
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2024-09-25 00:40:37 +0530

    Time: 4 ms (6.16%), Space: 6 MB (100%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -3,22 +3,26 @@ public:
     int compareVersion(string version1, string version2) {
         int i=0, j=0;
         while(i<version1.size() || j<version2.size()){
-            int n1=0, n2=0;
-
+            long long n1= 0, n2= 0;
             while(i<version1.size() && version1[i]!='.'){
-                n1= n1*10 + (version1[i]-'0');
+                n1= n1*10 + version1[i]-'0';
                 i++;
             }
-
+            
             while(j<version2.size() && version2[j]!='.'){
-                n2= n2*10 + (version2[j]-'0');
+                n2= n2*10 + version2[j]-'0';
                 j++;
             }
 
-            if(n1<n2) return -1;
-            else if(n1>n2) return 1;
-            else {i++; j++;}
+            if(n1>n2) return 1;
+            else if(n1<n2) return -1;
+            else{
+                i++;
+                j++;
+            }
         }
         return 0;
     }
 };
+// TC: O(m+n), SC: O(1)
+// https://youtu.be/Z4Af1_t7wzk
