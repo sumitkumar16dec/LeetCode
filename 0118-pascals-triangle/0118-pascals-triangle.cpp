dc9fcf5162a6632bdceeb311d74b4e2c9c0547f0
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2024-12-01 13:56:43 +0530

    Time: 0 ms (100%), Space: 8 MB (91.21%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -6,7 +6,7 @@ public:
             ans[i].resize(i+1);
             ans[i][0]=ans[i][i]=1;
             for(int j=1;j<i;j++){
-                ans[i][j]= ans[i-1][j-1] + ans[i-1][j];
+                ans[i][j]= ans[i-1][j-1]+ans[i-1][j];
             }
         }
         return ans;
