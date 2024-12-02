d0e92baa3ea156d891ebb4f7e34a87001574897b
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2024-12-03 00:32:51 +0530

    Time: 4 ms (100%), Space: 14.8 MB (52.37%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int i1=-1, i2=-1, n=nums.size();
-        //step1: find jisko swap karna hai
+        int n= nums.size(), ind1=-1, ind2=-1;
         for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]) {i1=i; break;}
+            if(nums[i]<nums[i+1]) {ind1=i; break;}
         }
 
-        //step2: find jisse swap karna hai
-        if(i1!=-1){
+        if(ind1!=-1){
             for(int i=n-1;i>=0;i--){
-                if(nums[i]>nums[i1]) {i2=i; break;}
+                if(nums[i]>nums[ind1]) {ind2=i; break;}
             }
         }
 
-        if(i1!=-1) swap(nums[i1], nums[i2]);
+        if(ind1!=-1) swap(nums[ind1], nums[ind2]);
 
-        reverse(nums.begin()+i1+1, nums.end());
+        reverse(nums.begin()+ind1+1, nums.end());
     }
 };
 // TC: O(n), SC: O(1)
