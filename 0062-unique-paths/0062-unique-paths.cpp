0fa696ea018b2d2cd818a384f377941ec0aaf08b
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2024-12-07 23:36:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int N=m+n-2, r=m-1;
-        int ans=1;
+        int N= m+n-2, r=m-1, ans=1;
         for(int i=1;i<=r;i++){
             ans= (ans*(N-r+i))/i;
         }
