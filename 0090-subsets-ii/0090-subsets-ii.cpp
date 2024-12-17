287cc5d09e303c07bf683bc46eb7f58f23a0d526
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2024-12-17 10:53:54 +0530

    Time: 5 ms (16.7%), Space: 9.4 MB (69.53%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,23 +1,24 @@
 class Solution {
-public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        vector<int> arr;
-        func(0, ans, arr, nums);
-        return ans;
-    }
-
-    void func(int ind, vector<vector<int>> &ans, vector<int> &arr, vector<int> &nums){
+private:
+    void func(int ind, vector<int> &arr, vector<vector<int>> &ans, vector<int> nums){
         ans.push_back(arr);
 
         for(int i=ind;i<nums.size();i++){
             if(i>ind && nums[i]==nums[i-1]) continue;
 
             arr.push_back(nums[i]);
-            func(i+1, ans, arr, nums);
+            func(i+1, arr, ans, nums);
             arr.pop_back();
         }
+    }
+
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> arr;
+        sort(nums.begin(), nums.end());
+        func(0, arr, ans, nums);
+        return ans;
     }
 };
 // TC: O(2^n * k)
