9351cbd71b87dbfaaa81dd0a563473a46dd42cd9
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-01-02 00:43:59 +0530

    Time: 0 ms (100%), Space: 15.3 MB (19.7%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,14 +4,14 @@ public:
         int low=0, high=nums.size()-1;
         while(low<=high){
             int mid= (low+high)>>1;
-            if(nums[mid]==target) return mid;
-            else if(nums[low]<=nums[mid]){   // left half is sorted
-                if(nums[low]<=target && target<=nums[mid]) high=mid-1;
+            if(target==nums[mid]) return mid;
+            if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target<=nums[mid]) high=mid-1;
                 else low=mid+1;
             }
-            else{   // right half is sorted
-                if(nums[mid]<=target && target<=nums[high]) low=mid+1;
-                else high=mid-1;
+            else{
+                if(target>=nums[mid] && target<=nums[high]) low=mid+1;
+                else high= mid-1;
             }
         }
         return -1;
