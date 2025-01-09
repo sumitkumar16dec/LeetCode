e7be9ce822454618bf028c19442f3e2b42584a29
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-01-10 01:03:50 +0530

    Time: 64 ms (37.92%), Space: 61.6 MB (48.86%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,32 +1,32 @@
 class Solution {
 private:
     bool isPalindrome(int st, int en, string s){
-        while(st<en){
+        while(st<=en){
             if(s[st++]!=s[en--]) return false;
         }
         return true;
     }
 
-    void func(int ind, vector<string> &arr, vector<vector<string>> &ans, string s){
+    void func(int ind, string s, vector<string> &temp, vector<vector<string>> &ans){
         if(ind==s.size()){
-            ans.push_back(arr);
+            ans.push_back(temp);
             return;
         }
 
         for(int i=ind;i<s.size();i++){
             if(isPalindrome(ind, i, s)){
-                arr.push_back(s.substr(ind, i-ind+1));
-                func(i+1, arr, ans, s);
-                arr.pop_back();
+                temp.push_back(s.substr(ind,i-ind+1));
+                func(i+1, s, temp, ans);
+                temp.pop_back();
             }
         }
+        
     }
-
 public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
-        vector<string> arr;
-        func(0, arr, ans, s);
+        vector<string> temp;
+        func(0, s, temp, ans);
         return ans;
     }
 };
