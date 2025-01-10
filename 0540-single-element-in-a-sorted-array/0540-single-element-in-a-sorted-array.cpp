598c1c345cc8cfb3d774298a13f7828bf5fec7ae
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-01-10 23:51:41 +0530

    Time: 0 ms (100%), Space: 26.2 MB (53.75%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,8 +4,8 @@ public:
         int low=0, high=nums.size()-2;
         while(low<=high){
             int mid= (low+high)>>1;
-            if(nums[mid]==nums[mid^1]) low=mid+1;   // non duplicate is in right side
-            else high=mid-1;    // non d is in left
+            if(nums[mid]==nums[mid^1]) low=mid+1;
+            else high=mid-1;
         }
         return nums[low];
     }
