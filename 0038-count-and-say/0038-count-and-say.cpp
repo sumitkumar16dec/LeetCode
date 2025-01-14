0b1f7bc16edcae673d450cd50bf4a284899caa42
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-01-14 21:41:16 +0530

    Time: 3 ms (84.49%), Space: 9.9 MB (64.67%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -4,21 +4,22 @@ public:
         if(n==1) return "1";
         if(n==2) return "11";
         string s= "11";
-        for(int i=3;i<=n;i++){
-            string t= "";
-            s= s+"$";
+        for(int i=3;i<=n;i++){              // O(n)
+            s += "$";
+            string t = "";
             int c=1;
-            for(int i=1;i<s.size();i++){
-                if(s[i]!=s[i-1]){
-                    t= t+to_string(c);
-                    t= t+s[i-1];
+            for(int j=1;j<s.size();j++){    // O(m)     
+                if(s[j]==s[j-1]) c++;
+                else{
+                    t += to_string(c);
+                    t += s[j-1];
                     c=1;
                 }
-                else c++;
             }
             s= t;
         }
         return s;
     }
 };
+// TC: O(n*m), SC: O(1)
 // https://www.youtube.com/watch?v=1YUqtoT9YoE
