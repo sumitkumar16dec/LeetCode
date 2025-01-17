d8dea48837e3572ec516addc7205411495bbc12c
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-01-17 16:45:13 +0530

    Time: 0 ms (100%), Space: 10.2 MB (97.45%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -12,25 +12,31 @@
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> postorder;
-        if(root==NULL) return postorder;
-        stack<TreeNode*> st1, st2;
-        st1.push(root);
-        while(!st1.empty()){
-            TreeNode *temp= st1.top();
-            st1.pop();
-            st2.push(temp);
-            if(temp->left) st1.push(temp->left);
-            if(temp->right) st1.push(temp->right);
+        vector<int> ans;
+        TreeNode *cur= root;
+        stack<TreeNode*> st;
+        while(cur || !st.empty()){
+            if(cur!=NULL){
+                st.push(cur);
+                cur= cur->left;
+            }
+            else{
+                TreeNode *temp= st.top()->right;
+                if(temp==NULL){
+                    temp= st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() && temp==st.top()->right){
+                        temp= st.top(); st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else cur= temp;
+            }
         }
-
-        while(!st2.empty()){
-            postorder.push_back(st2.top()->val);
-            st2.pop();
-        }
-        return postorder;
+        return ans;
     }
 };
 // Iterative way
-// TC: O(n), SC: O(2n)
+// TC: O(n), SC: O(n)
 // https://www.youtube.com/watch?v=NzIGLLwZBS8
