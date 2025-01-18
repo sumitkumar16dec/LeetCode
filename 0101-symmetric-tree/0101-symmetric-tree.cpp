ea4ce3c6dd16a027997ff78596d86dca40ecf0b5
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-01-19 00:39:45 +0530

    Time: 0 ms (100%), Space: 18.4 MB (72.81%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -10,18 +10,17 @@
  * };
  */
 class Solution {
-private:
-    bool sym(TreeNode *left, TreeNode *right){
-        if(left==NULL || right==NULL) return left==right;
+public:
+    bool sym(TreeNode *a, TreeNode *b){
+        if(a==NULL || b==NULL) return a==b;
 
-        return left->val==right->val 
-            && sym(left->left, right->right)
-            && sym(left->right, right->left);
+        return a->val==b->val
+            && sym(a->left,b->right)
+            && sym(a->right,b->left);
     }
 
-public:
     bool isSymmetric(TreeNode* root) {
-        return root==NULL || sym(root->left, root->right);
+        return sym(root->left,root->right);
     }
 };
 // TC: O(n) [tree traversal], SC: O(n) [only recursion stack space]
