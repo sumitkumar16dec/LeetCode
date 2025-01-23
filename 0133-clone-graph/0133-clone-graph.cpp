c093bef51435b4418a0f6aecc67aa800c766e04f
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-01-24 03:51:26 +0530

    Time: 4 ms (36.69%), Space: 12.5 MB (5.1%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -21,32 +21,30 @@ public:
 
 class Solution {
 private:
-    Node* dfs(Node* node, unordered_map<Node*,Node*> &mp){
-        Node* copy= new Node(node->val);
+    Node *dfs(Node *node, unordered_map<Node*,Node*> &mp){
+        Node *copy= new Node(node->val);
         mp[node]= copy;
 
-        vector<Node*> neighbour;
+        vector<Node*> copyNe;
         for(auto it: node->neighbors){
-            if(mp.find(it) != mp.end()) neighbour.push_back(mp[it]);
-
-            else neighbour.push_back(dfs(it,mp));
+            if(mp.find(it)!=mp.end()) copyNe.push_back(mp[it]);
+            else copyNe.push_back(dfs(it,mp));
         }
-        copy->neighbors= neighbour;
-
+        copy->neighbors= copyNe;
         return copy;
     }
 
 public:
     Node* cloneGraph(Node* node) {
         if(node==NULL) return NULL;
-        
-        if((node->neighbors).size()==0){
-            Node* copy= new Node(node->val);
+
+        if(node->neighbors.size()==0){
+            Node *copy= new Node(node->val);
             return copy;
         }
 
         unordered_map<Node*,Node*> mp;
-        return dfs(node, mp);
+        return dfs(node,mp);
     }
 };
 // TC: O(v+e), SC: O(v)
