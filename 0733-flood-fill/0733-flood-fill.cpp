3f551ef50adb64f6189dd43cf2e4e69f87d54abc
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-01-26 00:26:35 +0530

    Time: 0 ms (100%), Space: 17.9 MB (88.18%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,14 +1,14 @@
 class Solution {
 private:
-    void dfs(vector<vector<int>>& image, vector<vector<int>>& ans, int sr, int sc, int iniCol, int newCol, int delRow[], int delCol[], int n, int m){
-        ans[sr][sc]= newCol;
-        
-        for(int i=0;i<4;i++){
-            int row= sr+delRow[i];
-            int col= sc+delCol[i];
+    void dfs(vector<vector<int>> &image, int sr, int sc, int color, vector<vector<int>> &ans, int inicol, int delrow[], int delcol[], int n, int m){
+        ans[sr][sc]=color;
 
-            if(row>=0 && row<n && col>=0 && col<m && image[row][col]==iniCol && ans[row][col]!=newCol){
-                dfs(image, ans, row, col, iniCol, newCol, delRow, delCol, n, m);
+        for(int i=0;i<4;i++){
+            int row= sr+delrow[i];
+            int col= sc+delcol[i];
+
+            if(row>=0 && row<n && col>=0 && col<m && image[row][col]==inicol && ans[row][col]!=color){
+                dfs(image, row, col, color, ans, inicol, delrow, delcol, n, m);
             }
         }
     }
@@ -16,15 +16,12 @@ private:
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         vector<vector<int>> ans= image;
-        int iniCol= image[sr][sc];
-
-        int n= image.size();
-        int m= image[0].size();
-        int delRow[4]= {-1, 0, 1, 0};
-        int delCol[4]= {0, 1, 0, -1};
-        dfs(image, ans, sr, sc, iniCol, color, delRow, delCol, n, m);
-
-        return ans; 
+        int delrow[]= {-1, 0, 1, 0};
+        int delcol[]= {0, 1, 0, -1};
+        int inicol= image[sr][sc];
+        int n= image.size(), m= image[0].size();
+        dfs(image, sr, sc, color, ans, inicol, delrow, delcol, n, m);
+        return ans;
     }
 };
 // TC: O(n*m), SC: O(n*m)[ans matrix] + O(n*m)[recursion stack space] + O(n+m) [delRow,delCol] = O(2(n*m)+(n+m))
