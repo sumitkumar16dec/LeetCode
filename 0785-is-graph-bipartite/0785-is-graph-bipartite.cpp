7a228747bba622f52d0e56ca6bd1fe3579758402
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-01-27 01:04:51 +0530

    Time: 30 ms (5.71%), Space: 13.4 MB (100%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,22 +1,14 @@
 class Solution {
 private:
-    bool bfs(int node, int vis[], vector<vector<int>>& graph){
-        vis[node]= 0;
-        queue<int> q;
-        q.push(node);
-        
-        while(!q.empty()){
-            node= q.front();
-            q.pop();
-            
-            for(auto it: graph[node]){
-                
-                if(vis[it]==-1){
-                    vis[it] = !vis[node];
-                    q.push(it);
-                }
-                else if(vis[it]==vis[node]) return false;
+    bool dfs(int node, int color, int vis[], vector<vector<int>>& graph){
+        vis[node]= color;
+
+        for(auto it: graph[node]){
+
+            if(vis[it]==-1){
+                if(dfs(it, !color, vis, graph)==false) return false;
             }
+            else if(vis[it]==vis[node]) return false;
         }
         return true;
     }
@@ -29,11 +21,11 @@ public:
 	    
 	    for(int i=0;i<V;i++){
 	        if(vis[i]==-1){
-	            if(bfs(i, vis, graph)==0) return false;
+	            if(dfs(i, 0, vis, graph)==false) return false;
 	        }
 	    }
 	    return true;
     }
 };
-// TC: O(V+E), SC: O(2V)
-// https://youtu.be/-vu34sct1g8
+// TC: O(V+E), SC: O(V)
+// https://youtu.be/KG5YFfR0j8A
