7ee87f18b19322e659f82222cdf372f1db60a846
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-02-15 14:17:38 +0530

    Time: 92 ms (67%), Space: 10.2 MB (63.98%) - LeetHub

--- 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
@@ -7,8 +7,8 @@ private:
         for(int i=ind;i<str.size();i++){
             sum= sum*10 + (str[i]-'0');
             if(sum>target) break;
-            if(canPartition(str, i+1, target-sum)) return true;
-        }
+            if(canPartition(str,i+1,target-sum)) return true;
+        } 
         return false;
     }
 
@@ -17,8 +17,8 @@ public:
         int sum=0;
         for(int i=1;i<=n;i++){
             int sq= i*i;
-            string sqStr= to_string(sq);
-            if(canPartition(sqStr, 0, i)){
+            string str= to_string(sq);
+            if(canPartition(str,0,i)){
                 sum+= sq;
             }
         }
