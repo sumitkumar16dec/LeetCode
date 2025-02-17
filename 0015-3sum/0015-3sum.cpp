c5e48891917d39e8d2692f8e8519caf034440fff
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-02-18 00:20:51 +0530

    Time: 35 ms (98.84%), Space: 29 MB (67.25%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,8 +6,8 @@ public:
         int n = nums.size();
         for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-            int j=i+1, k=n-1;
             int left= 0-nums[i];
+            int j=i+1, k=n-1;
             while(j<k){
                 if(nums[j]+nums[k] < left) j++;
                 else if(nums[j]+nums[k] > left) k--;
