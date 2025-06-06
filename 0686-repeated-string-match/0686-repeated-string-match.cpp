23e2289daacda4d4c03b92bf51afac39a74b8aa8
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-06-07 02:01:43 +0530

    Time: 7 ms (45.41%), Space: 9.8 MB (71.29%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int repeatedStringMatch(string a, string b) {
-        string temp= a;
         int c=1;
-        while(temp.size() < b.size()) {temp +=a; c++;}
+        string temp= a;
+        while(temp.size()<b.size()){
+            c++;
+            temp+=a;
+        }
         if(temp.find(b)!=-1) return c;
 
         temp+=a;
         if(temp.find(b)!=-1) return c+1;
+
         return -1;
     }
 };
