848b6c10400387bb2985a5a00f06f5e15d55241d
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-06-07 18:36:07 +0530

    Time: 166 ms (13.15%), Space: 137.4 MB (95.39%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> ans;
-        deque<int> dq;      // SC: O(k)
+        deque<int> dq;      // O(k)
         for(int i=0;i<nums.size();i++){     // O(n+n)
-            // out of bound wale ko hatana
-            if(!dq.empty() && dq.front()==(i-k)) dq.pop_front();
-
-            // lesser than/ equal to elements ho hatana
+            if(!dq.empty() && dq.front()<=i-k) dq.pop_front();
             while(!dq.empty() && nums[dq.back()]<=nums[i]) dq.pop_back();
-
+            
             dq.push_back(i);
-
             if(i>=k-1) ans.push_back(nums[dq.front()]);
         }
-
         return ans;
     }
 };
-// TC: O(n+n), SC: O(k)
-// https://youtu.be/CZQGRp93K4k
+// TC: O(2n), SC: O(k)
+// https://www.youtube.com/watch?v=NwBvene4Imo
