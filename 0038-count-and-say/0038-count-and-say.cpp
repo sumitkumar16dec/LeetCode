7db0b898424ae09a60ec66950f153b5346a5d053
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-06-08 00:16:09 +0530

    Time: 3 ms (82.15%), Space: 9.7 MB (88%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -3,17 +3,17 @@ public:
     string countAndSay(int n) {
         if(n==1) return "1";
         if(n==2) return "11";
+
         string s= "11";
-        
         for(int i=3;i<=n;i++){
-            s=s+'$';
-            int c=1;
+            s+='$';
+            int cnt=1;
             string temp= "";
             for(int j=1;j<s.size();j++){
-                if(s[j]==s[j-1]) c++;
+                if(s[j]==s[j-1]) cnt++;
                 else{
-                    temp+=to_string(c)+s[j-1];
-                    c=1;
+                    temp+= to_string(cnt)+s[j-1];
+                    cnt=1;
                 }
             }
             s= temp;
