3dec7b049eb2d8e019210851ef5bf10237b5739d
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-06-10 19:57:31 +0530

    Time: 374 ms (49.58%), Space: 8.7 MB (88.12%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,19 +1,14 @@
 class Solution {
-public:
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
-    }
-
 private:
     bool solve(vector<vector<char>> &board){
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
                 if(board[i][j]=='.'){
                     for(char c='1';c<='9';c++){
                         if(isValid(board, i, j, c)){
-                            board[i][j]= c;
+                            board[i][j] = c;
                             if(solve(board)==true) return true;
-                            else board[i][j]='.';
+                            board[i][j] = '.';
                         }
                     }
                     return false;
@@ -27,9 +22,15 @@ private:
         for(int i=0;i<9;i++){
             if(board[row][i]==c) return false;
             if(board[i][col]==c) return false;
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c) return false;
+            if(board[3*(row/3) + (i/3)][3*(col/3) + (i%3)]==c) return false;
         }
         return true;
     }
+
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
 };
+// TC: \U0001d442(\U0001d45b^(\U0001d45b^2+1)), SC: O(n^2)
 // https://youtu.be/FWAIf_EVUKE
