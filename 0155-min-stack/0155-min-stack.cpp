27e05291d116c3969287a3ddd17cfc65b276e93f
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-06-11 23:04:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,36 +1,33 @@
 class MinStack {
 public:
     stack<long> st;
-    long mini=INT_MAX;
+    long mini;
     MinStack() {
+        while(!st.empty()) st.pop();
+        mini= INT_MAX;
     }
     
-    void push(int value) {
-        long val= value;
-        if(st.empty()){
-            mini= val;
-            st.push(val);
-        }
+    void push(int val) {
+        if(val >= mini) st.push(val);
         else{
-            if(val>=mini) st.push(val);
-            else{
-                st.push((2*val)-mini);
-                mini= val;
-            }
+            st.push(2*val - mini);
+            mini = val;
         }
     }
     
     void pop() {
         if(st.empty()) return;
-        if(st.top()<mini) mini= 2*mini - st.top();
-        st.pop();
+        if(st.top()>mini) st.pop();
+        else{
+            mini= 2*mini - st.top();
+            st.pop();
+        }
     }
     
     int top() {
         if(st.empty()) return -1;
-        long top= st.top();
-        if(top<mini) return mini;
-        else return top;
+        if(st.top()>=mini) return st.top();
+        else return mini;
     }
     
     int getMin() {
