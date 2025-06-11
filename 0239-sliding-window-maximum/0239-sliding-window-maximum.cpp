47aeebf109319466180c391e8eb24d39b571a5b6
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-06-11 22:37:56 +0530

    Time: 25 ms (53.47%), Space: 139.3 MB (37.63%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> ans;
-        deque<int> dq;      // O(k)
-        for(int i=0;i<nums.size();i++){     // O(n+n)
+        vector<int> ans;        // SC: O(n-k)
+        int n= nums.size();
+        deque<int> dq;          // SC: O(k)
+        for(int i=0;i<n;i++){
             if(!dq.empty() && dq.front()<=i-k) dq.pop_front();
             while(!dq.empty() && nums[dq.back()]<=nums[i]) dq.pop_back();
-            
             dq.push_back(i);
             if(i>=k-1) ans.push_back(nums[dq.front()]);
         }
         return ans;
     }
 };
-// TC: O(2n), SC: O(k)
+// TC: O(2n) (n for for-loop & n for push-pop for n-elements), SC: O(n)
 // https://www.youtube.com/watch?v=NwBvene4Imo
