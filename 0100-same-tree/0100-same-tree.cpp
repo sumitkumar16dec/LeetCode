d4f30cbd7a1f2fe5df26f786bd0e5ee3335fbdbd
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-06-13 02:00:11 +0530

    Time: 0 ms (100%), Space: 12.8 MB (77.87%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,8 +15,8 @@ public:
         if(p==NULL || q==NULL) return p==q;
         if(p->val!=q->val) return false;
 
-        return isSameTree(p->right, q->right)
-            && isSameTree(p->left, q->left);
+        return isSameTree(p->left,q->left)
+            && isSameTree(p->right,q->right);
     }
 };
 // TC: O(n), SC: O(n)
