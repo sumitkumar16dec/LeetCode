1179cb89e5ad9b0ec3c9f34a08d0044353eee388
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-06-16 00:01:44 +0530

    Time: 0 ms (100%), Space: 21.9 MB (75.67%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -13,8 +13,9 @@ class Solution {
 private:
     bool check(TreeNode *root, long lb, long ub){
         if(root==NULL) return true;
-
+        
         if(root->val<=lb || root->val>=ub) return false;
+
         return check(root->left, lb, root->val)
             && check(root->right, root->val, ub);
     }
