b7245ab39d03edac775fbae204df6615f9f29ffb
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-06-17 09:48:59 +0530

    Time: 0 ms (100%), Space: 13 MB (15.18%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -13,11 +13,11 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==NULL || q==NULL) return p==q;
-        if(p->val!=q->val) return false;
 
-        return isSameTree(p->left,q->left)
-            && isSameTree(p->right,q->right);
+        return p->val==q->val
+            && isSameTree(p->left, q->left)
+            && isSameTree(p->right, q->right);
     }
 };
-// TC: O(n), SC: O(n)
+// TC: O(n), SC: O(n) [recursion stack space]
 // https://youtu.be/BhuvF_-PWS0
