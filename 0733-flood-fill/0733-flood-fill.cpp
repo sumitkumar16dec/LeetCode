dfb76f5ed99c27dde39dfac725705578bb828c9a
Sumit Kumar <77963232+sumitkumar16dec@users.noreply.github.com>
2025-06-24 10:18:55 +0530

    Time: 0 ms (100%), Space: 18 MB (77.94%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,14 +1,14 @@
 class Solution {
 private:
-    void dfs(vector<vector<int>> &image, int sr, int sc, int color, vector<vector<int>> &ans, int inicol, int delrow[], int delcol[], int n, int m){
-        ans[sr][sc]=color;
+    void dfs(vector<vector<int>> &image, int sr, int sc, int color, int iniColor, int delrow[], int delcol[], int n, int m, vector<vector<int>> &ans){
+        ans[sr][sc]= color;
 
         for(int i=0;i<4;i++){
             int row= sr+delrow[i];
             int col= sc+delcol[i];
 
-            if(row>=0 && row<n && col>=0 && col<m && image[row][col]==inicol && ans[row][col]!=color){
-                dfs(image, row, col, color, ans, inicol, delrow, delcol, n, m);
+            if(row>=0 && row<n && col>=0 && col<m && image[row][col]==iniColor && ans[row][col]!=color){
+                dfs(image, row, col, color, iniColor, delrow, delcol, n, m, ans);
             }
         }
     }
@@ -18,9 +18,9 @@ public:
         vector<vector<int>> ans= image;
         int delrow[]= {-1, 0, 1, 0};
         int delcol[]= {0, 1, 0, -1};
-        int inicol= image[sr][sc];
+        int iniColor= image[sr][sc];
         int n= image.size(), m= image[0].size();
-        dfs(image, sr, sc, color, ans, inicol, delrow, delcol, n, m);
+        dfs(image, sr, sc, color, iniColor, delrow, delcol, n, m, ans);
         return ans;
     }
 };
