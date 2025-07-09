223e89f1187eb425da108e76b9df45b346320668
Sumit Kumar <sk1664839@gmail.com>
2025-07-09 22:49:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -1,34 +1,32 @@
 class Solution {
   public:
-    void dfs(int node, stack<int> &s, int vis[], vector<vector<int>> &adj){
+    void dfs(int node, stack<int> &st, int vis[], vector<vector<int>> &adj){
         vis[node] = 1;
-        
         for(int i: adj[node]){
-            if(!vis[i]) dfs(i, s, vis, adj);
+            if(!vis[i]) dfs(i, st, vis, adj);
         }
-        s.push(node);
+        st.push(node);
     }
   
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
-        // code here
         vector<vector<int>> adj(V);
         for(auto it: edges){
             adj[it[0]].push_back(it[1]);
         }
         
         vector<int> ans;
-        stack<int> s;
+        stack<int> st;
         int vis[V]= {0};
         for(int i=0;i<V;i++){
-            if(!vis[i]) dfs(i, s, vis, adj);
+            if(!vis[i]) dfs(i, st, vis, adj);
         }
         
-        while(!s.empty()){
-            ans.push_back(s.top());
-            s.pop();
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
         }
         return ans;
     }
 };
-// TC: O(v+e), SC: O(2n)
+// TC: O(v+e), SC: O(2v)
 // https://youtu.be/5lZ0iJMrUMk
