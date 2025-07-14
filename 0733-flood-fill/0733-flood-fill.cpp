e7856bff1f6395d8ff4ac4915eb8c59bb7203f1e
Sumit Kumar <sk1664839@gmail.com>
2025-07-14 22:59:50 +0530

    Time: 6 ms (2.28%), Space: 17.9 MB (90.47%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,23 +1,22 @@
 class Solution {
-private:
-    void recursion(vector<vector<int>>& image, int sr, int sc, int color, int iniCol, vector<vector<int>>& ans, int delrow[], int delcol[], int n, int m){
-        ans[sr][sc]= color;
+public:
+    void recursion(vector<vector<int>> &image, int sr, int sc, int color, vector<vector<int>> &ans, int delrow[], int delcol[], int n, int m, int iniColor){
+        ans[sr][sc]=color;
+
         for(int i=0;i<4;i++){
             int row= sr+delrow[i];
             int col= sc+delcol[i];
-            if(row>=0 && row<n && col>=0 && col<m && image[row][col]==iniCol && ans[row][col]!=color){
+            if(row>=0 && row<n && col>=0 && col<m && image[row][col]==iniColor && ans[row][col]!=color){
                 ans[row][col]=color;
-                recursion(image, row, col, color, iniCol, ans, delrow, delcol, n, m);
+                recursion(image, row, col, color, ans, delrow, delcol, n, m, iniColor);
             }
         }
     }
 
-public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         vector<vector<int>> ans= image;
-        int delrow[4]= {-1, 0, 1, 0}, delcol[4]= {0, 1, 0, -1}, n= image.size(), m= image[0].size();
-        int iniCol= image[sr][sc];
-        recursion(image, sr, sc, color, iniCol, ans, delrow, delcol, n, m);
+        int delrow[4]= {-1,0,1,0}, delcol[4]={0,1,0,-1}, n=image.size(), m=image[0].size(), iniColor= image[sr][sc];
+        recursion(image, sr, sc, color, ans, delrow, delcol, n, m, iniColor);
         return ans;
     }
 };
