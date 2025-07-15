1df5cda97a36e49ef21cc8c82821bdcb78720418
Sumit Kumar <sk1664839@gmail.com>
2025-07-15 12:09:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -4,19 +4,19 @@ class Solution {
         vis[row][col]=1;
         queue<pair<int,int>> q;
         q.push({row,col});
-        int delrow[8]= {-1,-1,0,1,1,1,0,-1};
-        int delcol[8]= {0,1,1,1,0,-1,-1,-1};
         while(!q.empty()){
             int trow= q.front().first;
             int tcol= q.front().second;
             q.pop();
             
-            for(int i=0;i<8;i++){
-                int nrow= trow+delrow[i];
-                int ncol= tcol+delcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='L' && !vis[nrow][ncol]){
-                    vis[nrow][ncol]=1;
-                    q.push({nrow,ncol});
+            for(int i=-1;i<=1;i++){
+                for(int j=-1;j<=1;j++){
+                    int nrow= trow+i;
+                    int ncol= tcol+j;
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='L' && !vis[nrow][ncol]){
+                        vis[nrow][ncol]=1;
+                        q.push({nrow,ncol});
+                    }
                 }
             }
         }
