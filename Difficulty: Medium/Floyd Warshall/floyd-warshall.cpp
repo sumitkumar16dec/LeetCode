474762736e75aef66025ef9388287dbcf93bde4f
Sumit Kumar <sk1664839@gmail.com>
2025-07-19 17:48:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
@@ -1,68 +1,22 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function template for C++
 
 class Solution {
   public:
-    void shortestDistance(vector<vector<int>>& mat) {
-        int n= mat.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(mat[i][j]==-1) mat[i][j]=1e9;
-            }
-            mat[i][i]=0;
-        }
-        
+    void floydWarshall(vector<vector<int>> &dist) {
+        int n= dist.size();
         for(int k=0;k<n;k++){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
-                    mat[i][j]= min(mat[i][j], mat[i][k]+mat[k][j]);
+                    if(dist[i][k]!=1e8 && dist[k][j]!=1e8)
+                        dist[i][j]= min(dist[i][j], dist[i][k]+dist[k][j]);
                 }
             }
         }
-        
+        // Extra
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(mat[i][j]==1e9) mat[i][j]=-1;
-            }
+            if(dist[i][i]<0) cout<<"negative weight cycle";
         }
     }
 };
-// TC: O(n^3), SC: O(n^2) since working on matrix
+// TC: O(V^3), SC: O(V^2) since working on matrix
 // https://youtu.be/YbY8cVwWAvw
-
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> matrix(n, vector<int>(n, -1));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> matrix[i][j];
-            }
-        }
-        Solution obj;
-        obj.shortestDistance(matrix);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cout << matrix[i][j] << " ";
-            }
-            cout << "\n";
-        }
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
