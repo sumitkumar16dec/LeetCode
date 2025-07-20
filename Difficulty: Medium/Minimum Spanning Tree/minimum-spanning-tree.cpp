43ce8f18b475f25b16971b236b05c0f8c879fc78
Sumit Kumar <sk1664839@gmail.com>
2025-07-20 14:10:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -2,11 +2,9 @@ class DisjointSet{
     vector<int> parent, size;
   public:
     DisjointSet(int n){
-        parent.resize(n);
         size.resize(n,1);
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-        }
+        parent.resize(n);
+        for(int i=0;i<n;i++) parent[i]=i;
     }
     
     int ultPar(int node){
@@ -15,15 +13,14 @@ class DisjointSet{
     }
     
     void unionBySize(int u, int v){
-        int up_u= ultPar(u), up_v= ultPar(v);
-        if(up_u == up_v) return;
+        int up_u = ultPar(u), up_v = ultPar(v);
         if(size[up_u] < size[up_v]){
             parent[up_u]= up_v;
-            size[up_v]+=size[up_u];
+            size[up_v]+= size[up_u];
         }
         else{
-            parent[up_v]= up_u;
-            size[up_u]+=size[up_v];
+            parent[up_v] = up_u;
+            size[up_u]+= size[up_v];
         }
     }
 };
@@ -32,8 +29,8 @@ class Solution {
   public:
     // Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[]) {
-        vector<pair<int,pair<int,int>>> edges;  // SC: O(e)
-        for(int i=0;i<V;i++){                   // O(v+e)
+        vector<pair<int,pair<int,int>>> edges;      // SC: O(e)
+        for(int i=0;i<V;i++){                       // O(v+e)
             for(auto it: adj[i]){
                 int u= i;
                 int v= it[0];
@@ -41,16 +38,15 @@ class Solution {
                 edges.push_back({wt,{u,v}});
             }
         }
-        sort(edges.begin(), edges.end());       // O(eloge)
+        
+        sort(edges.begin(), edges.end());           // O(eloge)
         
         int mstWt=0;
         DisjointSet ds(V);
-        for(auto it: edges){                    // O(e *4alpha)
-            int wt= it.first;
-            int u= it.second.first;
-            int v= it.second.second;
+        for(auto it: edges){                        // O(e 4alpha)
+            int wt= it.first, u= it.second.first, v= it.second.second;
             if(ds.ultPar(u)!=ds.ultPar(v)){
-                mstWt+= wt;
+                mstWt+=wt;
                 ds.unionBySize(u,v);
             }
         }
