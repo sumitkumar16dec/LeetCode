bdd379ac1067500d6de1356ebf9c9685cc6218e0
Sumit Kumar <sk1664839@gmail.com>
2025-07-20 15:55:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Strongly Connected/strongly-connected.cpp
+++ Difficulty: Medium/Strongly Connected/strongly-connected.cpp
@@ -1,43 +1,42 @@
 //Position this line where user code will be pasted.
 class Solution {
-  private:
-    void dfs(int node, vector<int> &vis, stack<int> &st, vector<vector<int>> &adj){
+  public:
+    void dfs(int node, vector<int> &vis, vector<vector<int>> &adj, stack<int> &st){
         vis[node]=1;
-        for(int i: adj[node]){
-            if(!vis[i]) dfs(i, vis, st, adj);
+        for(int it: adj[node]){
+            if(!vis[it]) dfs(it, vis, adj, st);
         }
         st.push(node);
     }
     
-    void dfs2(int node, vector<int> &vis, vector<vector<int>> &adjT){
+    void dfs2(int node, vector<int> &vis, vector<vector<int>> &adj){
         vis[node]=1;
-        for(int i: adjT[node]){
-            if(!vis[i]) dfs2(i, vis, adjT);
+        for(int it: adj[node]){
+            if(!vis[it]) dfs2(it, vis, adj);
         }
     }
-    
-  public:
+  
     int kosaraju(vector<vector<int>> &adj) {
         // step 1: sort acd to finishing time
         stack<int> st;                          // SC: O(v)
-        int v= adj.size();
-        vector<int> vis(v,0);                   // SC: O(v)
-        for(int i=0;i<v;i++){
-            if(!vis[i]) dfs(i, vis, st, adj);
+        int n= adj.size();
+        vector<int> vis(n,0);                   // SC: O(v)
+        for(int i=0;i<n;i++){                   // O(v+e)
+            if(!vis[i]) dfs(i, vis, adj, st);
         }
         
         // step 2: transpose edges
-        vector<vector<int>> adjT(v);            // SC: O(v+e)
-        for(int i=0;i<v;i++){
+        vector<vector<int>> adjT(n);            // SC: O(v+e)
+        for(int i=0;i<n;i++){                   // O(v+e)
             vis[i]=0;
             for(int it: adj[i]){
                 adjT[it].push_back(i);
             }
         }
         
-        // step 3: dfs from finishing node
+        // step 3: count scc
         int scc=0;
-        while(!st.empty()){
+        while(!st.empty()){                     // O(v+e)
             int node= st.top(); st.pop();
             if(!vis[node]){
                 scc++;
@@ -47,4 +46,5 @@ class Solution {
         return scc;
     }
 };
-// TC: O(v+e), SC: O(v+e)
+// O(3(V+E)), SC: O(V+E + 2V)
+// https://youtu.be/R6uoSjZ2imo
