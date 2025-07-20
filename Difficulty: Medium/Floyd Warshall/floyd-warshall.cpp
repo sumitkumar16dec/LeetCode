759502ceaa3a45fee20e82cf9b9dfb774605bbf4
Sumit Kumar <sk1664839@gmail.com>
2025-07-20 20:04:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
@@ -7,14 +7,16 @@ class Solution {
         for(int k=0;k<n;k++){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
-                    if(dist[i][k]!=1e8 && dist[k][j]!=1e8)
+                    if(dist[i][k]!=1e8 && dist[k][j]!=1e8){
                         dist[i][j]= min(dist[i][j], dist[i][k]+dist[k][j]);
+                    }
                 }
             }
         }
+        
         // Extra
         for(int i=0;i<n;i++){
-            if(dist[i][i]<0) cout<<"negative weight cycle";
+            if(dist[i][i]<0) cout<<"Negative weight cycle";
         }
     }
 };
