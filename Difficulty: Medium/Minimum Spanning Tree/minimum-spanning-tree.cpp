555f41242b305e22bc76293493a0c1359fdffb2d
Sumit Kumar <sk1664839@gmail.com>
2025-07-20 13:18:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,28 +1,61 @@
+class DisjointSet{
+    vector<int> parent, size;
+  public:
+    DisjointSet(int n){
+        parent.resize(n);
+        size.resize(n,1);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+    }
+    
+    int ultPar(int node){
+        if(node==parent[node]) return node;
+        return parent[node] = ultPar(parent[node]);
+    }
+    
+    void unionBySize(int u, int v){
+        int up_u= ultPar(u), up_v= ultPar(v);
+        if(up_u == up_v) return;
+        if(size[up_u] < size[up_v]){
+            parent[up_u]= up_v;
+            size[up_v]+=size[up_u];
+        }
+        else{
+            parent[up_v]= up_u;
+            size[up_u]+=size[up_v];
+        }
+    }
+};
+
 class Solution {
   public:
     // Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[]) {
-        int sum=0;
-        vector<int> vis(V,0);
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minHeap;
-        minHeap.push({0,0});
-        while(!minHeap.empty()){
-            int wt= minHeap.top().first;
-            int node= minHeap.top().second;
-            minHeap.pop();
-            
-            if(!vis[node]){
-                sum+= wt;
-                vis[node]=1;
-                for(auto ne: adj[node]){
-                    int adjNode= ne[0];
-                    int edWt= ne[1];
-                    if(!vis[adjNode]) minHeap.push({edWt,adjNode});
-                }
+        vector<pair<int,pair<int,int>>> edges;  // SC: O(e)
+        for(int i=0;i<V;i++){                   // O(v+e)
+            for(auto it: adj[i]){
+                int u= i;
+                int v= it[0];
+                int wt= it[1];
+                edges.push_back({wt,{u,v}});
             }
         }
-        return sum;
+        sort(edges.begin(), edges.end());       // O(eloge)
+        
+        int mstWt=0;
+        DisjointSet ds(V);
+        for(auto it: edges){                    // O(e *4alpha)
+            int wt= it.first;
+            int u= it.second.first;
+            int v= it.second.second;
+            if(ds.ultPar(u)!=ds.ultPar(v)){
+                mstWt+= wt;
+                ds.unionBySize(u,v);
+            }
+        }
+        return mstWt;
     }
 };
-// TC: O(ElogV), SC: O(V+E)
-// https://youtu.be/mJcZjjKzeqk
+// TC: O(v+e + eloge + e), SC: O(E)
+// https://youtu.be/DMnDM_sxVig
