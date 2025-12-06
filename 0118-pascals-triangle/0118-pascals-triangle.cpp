7cbe2acc91f5ae550f2dce8bdcd3c855e8da849e
Sumit Kumar <sk1664839@gmail.com>
2025-12-07 01:26:30 +0530

    Time: 0 ms (100%), Space: 9.7 MB (32.57%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> generate(int n) {
-        vector<vector<int>> arr(n);
+        vector<vector<int>> ans(n);
         for(int i=0;i<n;i++){
-            arr[i].resize(i+1);
-            arr[i][0]=arr[i][i]=1;
-            for(int j=1;j<i;j++){
-                arr[i][j]= arr[i-1][j-1]+arr[i-1][j];
-            }
+            ans[i].resize(i+1);
+            ans[i][0]=ans[i][i]=1;
+            for(int j=1;j<i;j++) ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
         }
-        return arr;
+        return ans;
     }
 };
 // TC: O(n^2), SC: O(n^2)
