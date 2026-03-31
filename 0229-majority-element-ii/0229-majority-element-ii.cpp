bbc32df8df294d088bc1d6f7b022e16fb33f9c56
Sumit Kumar <sk1664839@gmail.com>
2026-04-01 02:40:28 +0530

    Time: 0 ms (100%), Space: 23.7 MB (64.52%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -4,13 +4,12 @@ public:
         vector<int> ans;
         int maj1=-1, maj2=-1, c1=0, c2=0;
         for(int i: nums){
-            if(maj1==i) c1++;
-            else if(maj2==i) c2++;
-            else if(c1==0) {maj1=i; c1++;}
+            if(i==maj1) c1++;
+            else if(i==maj2) c2++;
             else if(c2==0) {maj2=i; c2++;}
+            else if(c1==0) {maj1=i; c1++;}
             else {c1--; c2--;}
         }
-
         c1=0, c2=0;
         for(int i: nums){
             if(i==maj1) c1++;
